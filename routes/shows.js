49fa3429ff166360c8ed997a74ac00580d16e598
Alireza Noori <ali@rotageek.com>
2017-04-16 11:11:24 +1000

    Added post request is empty error handeling

--- routes/shows.js
+++ routes/shows.js
@@ -7,24 +7,29 @@ router
   .get('/', (req, res) => { // Just inform the user that they should post a valid json
     res.send('<h3>Please start by posting valid json data to get filtered response back</h3>')
   })
-  .post('/', (req, res) => { // Grab the incoming postd json and process it
-    // grab all the shows
-    let allShows = req.body.payload
-    // filter the shows to the ones with drm = true & episodeCount > 0
-    let filteredShows = allShows.filter(show => {
-      return show.drm && show.episodeCount > 0
-    })
-    // create a new json object to then populate and send back to the client side
-    let newshowsJson = {
-      response: [] // response key
-    }
-    for (let show of filteredShows) {
-      newshowsJson.response.push({
-        image: show.image.showImage,
-        slug: show.slug,
-        title: show.title
+  .post('/', (req, res, next) => { // Grab the incoming postd json and process it
+    if (Object.keys(req.body).length !== 0) { // do not process if object is empty
+      // grab all the shows
+      let allShows = req.body.payload
+      // filter the shows to the ones with drm = true & episodeCount > 0
+      let filteredShows = allShows.filter(show => {
+        return show.drm && show.episodeCount > 0
       })
+      // create a new json object to then populate and send back to the client side
+      let newshowsJson = {
+        response: [] // response key
+      }
+      for (let show of filteredShows) {
+        newshowsJson.response.push({
+          image: show.image.showImage,
+          slug: show.slug,
+          title: show.title
+        })
+      }
+      res.send(newshowsJson)
+    } else {
+      res.send({'error': 'post request is empty'})
     }
-    res.send(newshowsJson)
+
   })
 module.exports = router
